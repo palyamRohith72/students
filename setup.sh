154ef4524047102c8329fd3a751611c285d30e1c
palyamRohith72 <palyamrohith72@gmail.com>
2025-02-03 09:24:35 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y cmake build-essential libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev libboost-all-dev
-pip install -r requirements.txt
