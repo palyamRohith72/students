e11a82c26f74d1a6452d33b77d93eeb30802e80e
palyamRohith72 <palyamrohith72@gmail.com>
2025-02-03 09:24:23 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="your-app-name",
+    version="0.1",
+    install_requires=[
+        "streamlit",
+        "streamlit-option-menu",
+        "streamlit-extras",
+        "pymongo",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "seaborn",
+        "dlib==19.24.2",
+        "face_recognition",
+    ],
+)
